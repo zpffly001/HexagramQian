f0b9cc9b462a4ec828c9a7a3c9d3086694f72833
zhoupengfei <1813763637@qq.com>
2023-03-19 23:50:12 +0800

    [feat] 更新build.sh，添加子模块构建脚本

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+# 增加git子模块
+git submodule add -f  https://github.com/RWTH-HPC/CMake-codecov.git externals/CMake-codecov
+git submodule add -f https://github.com/google/googletest.git externals/googletest
+git submodule add -f https://github.com/Corvusoft/restbed.git externals/restbed
+git submodule add -f https://github.com/google/glog.git externals/glog
+
+# 删除git子模块
+rm -rf externals/CMake-codecov
+git rm --cached externals/CMake-codecov
+
+# 新仓库拉取子模块
 git submodule update --init --recursive
