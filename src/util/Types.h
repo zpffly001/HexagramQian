d70a4aa682197eb7ae26137efb6aba0007c1ce5a
zhoupengfei <1813763637@qq.com>
2023-03-21 00:41:38 +0800

    [add] 增加Types.h 定义一些数据类型

---
+++ src/util/Types.h
@@ -0,0 +1,33 @@
+#ifndef HEXAGRAMQIAN_TYPES_H
+#define HEXAGRAMQIAN_TYPES_H
+
+
+/**
+ * 所有类型的定义
+*/
+
+// 基本数据类型
+/**
+ * 无符号8位整数(字符)
+*/
+typedef unsigned char UChar;
+
+/**
+ * 无符号16位整数
+*/
+typedef unsigned short UShort;
+
+/**
+ * 无符号32位整数
+*/
+typedef unsigned int UInt;
+
+/**
+ * 无符号64位整数
+*/
+typedef unsigned long long ULong;
+
+
+
+
+#endif
