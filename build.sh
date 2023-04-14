97d95f02ef920d335cd6897a0c59e20911a1654c
zhoupengfei <44106812+zpffly001@users.noreply.github.com>
2023-04-14 17:43:07 +0800

    [feat] build.sh项目构建脚本优化

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git submodule add -f https://github.com/google/glog.git externals/glog
 rm -rf externals/CMake-codecov
 git rm --cached externals/CMake-codecov
 
-# 新仓库拉取子模块
+# 拉取子模块
 git submodule update --init --recursive
 
 # glog一开始没有logging.h，只有logging.h.in，那么你需要编译，编译后会生成对应的logging.h头文件
