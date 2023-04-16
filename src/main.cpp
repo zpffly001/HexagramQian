d589ab17bbd92a064f6fae77dedcb6d75319f828
zhoupengfei <1813763637@qq.com>
2023-04-17 00:22:01 +0800

    [feat] 主程序入口

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <network/App.h>
+#include <glog/logging.h>
+
+int main(const int, const char **argv)
+{
+    /* 日志配置 */
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
+    FLAGS_alsologtostderr = true;
+    FLAGS_minloglevel = 0;
+
+    /* 启动App */
+    App app(8080);
+    app.initialize();
+    app.start();
+    app.finalize();
+
+    return EXIT_SUCCESS;
+}
